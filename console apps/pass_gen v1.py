c5a4dd55d14f173393dde1b445a44baed77422db
XXHieXX <XXHieXX111@gmail.com>
2026-09-10 23:16:54 +0400

    pass_gen v1

---
+++ console apps/pass_gen v1.py
@@ -0,0 +1,36 @@
+import traceback
+import sys
+import string
+import random
+import time
+
+# User settings
+
+PASSWORD_LEN = 32
+PASSWORDS_COUNT = 16
+
+
+
+chars = string.ascii_letters + "0123456789"
+
+def get_random_char():
+    random_index = random.randint(0, len(chars) - 1)
+    return chars[random_index]
+
+def generate_password():
+    output = ""
+    
+    for _ in range(PASSWORD_LEN):
+        output += get_random_char()
+    
+    return output
+
+if __name__ == "__main__":
+    try:
+        for _ in range(PASSWORDS_COUNT):
+            password = generate_password()
+            print(password)
+    except Exception:
+        traceback.print_exc()
+        time.sleep(1)
+        sys.exit(-1)
