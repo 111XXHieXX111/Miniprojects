0535e6f5f3ce42db865f25c7799042c24abb5661
XXHieXX <XXHieXX111@gmail.com>
2026-09-10 23:21:36 +0400

    pass_gen v2

---
+++ console apps/pass_gen v2.py
@@ -0,0 +1,36 @@
+import traceback
+import sys
+import string
+import secrets
+import time
+
+# User settings
+
+PASSWORD_LEN = 32
+PASSWORDS_COUNT = 16
+USE_PUNCT_CHARS = True
+
+
+
+chars = string.ascii_letters + string.digits
+
+if USE_PUNCT_CHARS:
+    chars += string.punctuation
+
+def generate_password():
+    output = ""
+    
+    for _ in range(PASSWORD_LEN):
+        output += secrets.choice(chars)
+    
+    return output
+
+if __name__ == "__main__":
+    try:
+        for _ in range(PASSWORDS_COUNT):
+            password = generate_password()
+            print(password)
+    except Exception:
+        traceback.print_exc()
+        time.sleep(1)
+        sys.exit(-1)
